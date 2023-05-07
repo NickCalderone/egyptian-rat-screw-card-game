2371553c89a612f97bcd5a5305942cce3056b1d3
NickCalderone <calderonenick@gmail.com>
2023-05-07 16:35:27 -0600

    updates

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,28 @@
 <script setup>
-import { ref } from 'vue';
-import { fetchDeck } from "./utility"
 
+import { ref } from 'vue'
+import { fetchDeck } from './utility'
+import UserCreator from './components/userCreator.vue'
+import { useFetch } from './utility.js'
+
+let name = ref(null)
 let deck_id = ref(null)
-let players = ref(2)
+let error = ref(null)
 
-fetchDeck(deck_id)
-function test(){
-  fetchDeck(deck_id);
+let players = ref([]);
+
+function addPlayer(name)
+{
+  players.value.push(name)
+}
+
+async function newGame()
+{
+  if (players.value.length > 0)
+  {
+    let data = await useFetch("new/shuffle/?deck_count=1")
+    deck_id.value = data.deck_id;
+  }
 }
 
 </script>
@@ -21,28 +36,84 @@ function test(){
 
   <main>
     <div class="settings">
-      <h2>Radio</h2>
-      <input type="radio" id="two" value="2" v-model="players">
-      <label for="two">Two</label>
-      <br>
-      <input type="radio" id="three" value="3" v-model="players">
-      <label for="three">Three</label>
-      <br>
-      <input type="radio" id="four" value="4" v-model="players">
-      <label for="four">Four</label>
-      <br>
-      <input type="radio" id="five" value="5" v-model="players">
-      <label for="five">five</label>
-      <br>
-      <span>Picked: {{ players }}</span>
-      <code>{{ deck_id }}</code>
-      <button @click="test">fetchDeck</button>
+      <input v-model="name" placeholder="Name" />
+      <button @click="addPlayer(name)">make user</button>
+      <br />
+      <button @click="newGame">Start a New Game</button>
+    </div>
+    <div v-if="error">Deck ID: {{ error }}</div>
+    <div v-else-if="deck_id">Deck ID: {{ deck_id }}</div>
+    <div v-else>Start a new game</div>
+
+    <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
+    <div v-else-if="deck_id">
+      Data loaded:
+      <pre>{{ deck_id }}</pre>
+    </div>
+    <div v-else>Loading...</div>
+
+    <div v-if="players.length > 0" class="field">
+      <div v-for="player in players" class="player-area">
+        <p>{{ player }}</p>
+      </div>
+    </div>
+    <div v-else>
+      Add a player
     </div>
   </main>
 </template>
 
 <style scoped>
 .settings {
+  padding: 40px;
+  width: 600px;
+}
+
+.field {
+  display: flex;
+  justify-content: space-between;
+  max-width: 1200px;
+  background-color: blue;
+  margin: 0 auto;
+}
+
+.player-area {
+  text-align: center;
+  width: 100%;
+}
+
+.settings {
   background-color: red;
 }
 </style>
+
+<!-- // let gameInProgress, first, antepenultimate, penultimate, ultimate, advantage;
+
+// setup Game based on number of players
+// find dealer 
+// make decks using % and doling out cards
+
+// newTurn
+
+// playacard
+
+// check for face card
+
+// savechance
+// if they dont have a card to play
+
+// slap
+
+// check results
+// if ultimate equals penultiate, ultimate equals antepenultimate, ultimate equals first, ultimate + penultimate = 10
+
+// award cards
+// else 
+
+// penalty
+
+// endTurn
+
+// check if all cards are in someone's hand
+
+// cycle turns -->
--- client/src/utility.js
+++ client/src/utility.js
@@ -1,13 +1,14 @@
 import { watchEffect } from 'vue'
+import { ref } from 'vue'
 
-export function useFetch(url, deck_id)
-{
-	watchEffect(async () =>
-	{
-		let response = await (await fetch(url)).json()
-		deck_id.value = response.deck_id
-	})
-}
+// export function useFetch(url, deck_id)
+// {
+// 	watchEffect(async () =>
+// 	{
+// 		let response = await (await fetch(url)).json()
+// 		deck_id.value = response.deck_id
+// 	})
+// }
 
 export function fetchDeck(deck_id)
 {
@@ -16,4 +17,15 @@ export function fetchDeck(deck_id)
 		let response = await (await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")).json()
 		deck_id.value = response.deck_id
 	})
+}
+
+export async function useFetch(myUrl)
+{
+
+	let url = "https://www.deckofcardsapi.com/api/deck/" + myUrl
+
+	let response = await fetch(url)
+	let json = await response.json()
+
+	return json
 }
